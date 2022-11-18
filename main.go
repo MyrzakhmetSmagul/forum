6fce8ca3db2fcd1ca730154b320b83ed5bc0b525
Myrzakhmet <smagul.myrzakhmet@mail.ru>
2022-11-18 17:14:04 +0600

    forum

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	fmt.Println("hello")
+	database, err := sql.Open("sqlite3", "forum.db")
+	log.Println(err)
+	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS users (user_id INTEGER NOT NULL PRIMARY KEY, user_name TEXT NOT NULL, user_surname TEXT NOT NULL,user_gender TEXT NOT NULL, user_email TEXT, user_pwd TEXT NOT NULL)")
+	log.Println(err)
+	statement.Exec()
+	statement, err = database.Prepare("INSERT INTO users (user_id, user_name, user_surname, user_gender, user_email, user_pwd) VALUES (1, 'Myrzakhmet', 'Smagul', 'MALE', 'smagul.myrzakhmet@mail.ru', 'SHA256' )")
+	log.Println(err)
+	statement.Exec()
 
 }
