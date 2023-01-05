11fe8c442dbc8a1f4278c86366bf8f506ae76047
Myrzakhmet <smagul.myrzakhmet@mail.ru>
2023-01-05 16:15:48 +0600

    category

--- internal/repository/category.go
+++ internal/repository/category.go
@@ -8,14 +8,21 @@ import (
 )
 
 type CategoryQuery interface {
+	GetPostCategories(post *model.Post) error
+	SetPostCategory(post *model.Post) error
 }
 
 type categoryQuery struct {
 	db *sql.DB
 }
 
-func (c *categoryQuery) GetCategory(post *model.Post) error {
-	sqlStmt := `SELECT category_id FROM post_categories WHERE post_id=?`
+func (c *categoryQuery) GetPostCategories(post *model.Post) error {
+	sqlStmt := `SELECT pc.category_id, c.category
+	FROM categories c
+	INNER JOIN post_categories pc
+	ON pc.category_id = c.category_id
+	WHERE pc.post_id=?`
+
 	query, err := c.db.Prepare(sqlStmt)
 	if err != nil {
 		log.Println(err)
@@ -23,5 +30,41 @@ func (c *categoryQuery) GetCategory(post *model.Post) error {
 	}
 
 	defer query.Close()
+
+	rows, err := query.Query(post.ID)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var category model.Category
+		rows.Scan(&category.CategoryID, &category.Category)
+		post.Categories = append(post.Categories, category)
+	}
+
+	return nil
+}
+
+func (c *categoryQuery) SetPostCategory(post *model.Post) error {
+	sqlStmt := `INSERT INTO post_categories(post_id, category_id) VALUES(?,?)`
+	query, err := c.db.Prepare(sqlStmt)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	defer query.Close()
+
+	for i := 0; i < len(post.Categories); i++ {
+		_, err = query.Exec(post.Categories[i].CategoryID, post.Categories[i].Category)
+		if err != nil {
+			log.Printf("can't set post categories,\nPost_id = %d, category = %s, number = %d\n ERROR: %s", post.ID, post.Categories[i].Category, i, err.Error())
+			return err
+		}
+	}
+
 	return nil
 }
